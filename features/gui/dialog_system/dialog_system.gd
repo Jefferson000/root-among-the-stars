ea40698119905b7f9e3bef2cf970484e7363a57f
JeffersonTorresH <torres.hernandez.jefferson@gmail.com>
2025-09-30 16:12:12 -0600

    Dialog System First Part

---
+++ features/gui/dialog_system/dialog_system.gd
@@ -0,0 +1,30 @@
+@tool
+@icon("res://features/gui/dialog_system/icons/star_bubble.svg")
+class_name DialogSystemNode extends CanvasLayer
+
+var is_active : bool = false
+
+@onready var dialog_ui: Control = $DialogUI
+
+func _ready() -> void:
+	if Engine.is_editor_hint():
+		if get_viewport() is Window:
+			get_parent().remove_child( self )
+		return
+	hide_dialog()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if is_active == false:
+		return
+
+func show_dialog() -> void:
+	is_active = true
+	dialog_ui.visible = true
+	dialog_ui.process_mode = Node.PROCESS_MODE_ALWAYS
+	get_tree().paused = true
+
+func hide_dialog() -> void:
+	is_active = false
+	dialog_ui.visible = false
+	dialog_ui.process_mode = Node.PROCESS_MODE_DISABLED
+	get_tree().paused = false
