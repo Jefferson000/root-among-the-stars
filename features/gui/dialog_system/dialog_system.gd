9d365000d5add33bbb89d9ca339c89fdef81ed63
JeffersonTorresH <torres.hernandez.jefferson@gmail.com>
2025-10-01 23:45:24 -0600

    dialog fix

--- features/gui/dialog_system/dialog_system.gd
+++ features/gui/dialog_system/dialog_system.gd
@@ -114,6 +114,7 @@ func set_dialog_choice( _d : DialogChoice ) -> void:
 	if Engine.is_editor_hint():
 		return
 	await get_tree().process_frame
+	await get_tree().process_frame
 	choice_options.get_child( 0 ).grab_focus()
 
 func _dialog_choice_selected( _d : DialogBranch ) -> void:
